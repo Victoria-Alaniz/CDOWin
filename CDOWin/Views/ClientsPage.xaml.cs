6e721c41cb5e654cfe10b3e9958fce6d16d474ae
Jon <me@jonalaiz.com>
2025-11-20 04:59:15 -0600

    Added third column to ClientsPage

--- CDOWin/Views/ClientsPage.xaml.cs
+++ CDOWin/Views/ClientsPage.xaml.cs
@@ -1,6 +1,7 @@
 using CDO.Core.Services;
 using CDOWin.ViewModels;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -11,10 +12,33 @@ namespace CDOWin.Views;
 /// </summary>
 public sealed partial class ClientsPage : Page {
     public ClientsViewModel ViewModel { get; }
+
     public ClientsPage() {
         InitializeComponent();
 
         ViewModel = AppServices.ClientsViewModel;
         DataContext = ViewModel;
+    }
+
+    private void SelectionChanged(SelectorBar sender, SelectorBarSelectionChangedEventArgs args) {
+        SelectorBarItem selectedItem = sender.SelectedItem;
+        int currentSelectedIndex = sender.Items.IndexOf(selectedItem);
+        System.Type pageType;
+
+        switch (currentSelectedIndex) {
+            case 0:
+                pageType = typeof(SamplePage);
+                break;
+            case 1:
+                pageType = typeof(SamplePage);
+                break;
+            default:
+                pageType = typeof(SamplePage);
+                break;
+        }
+
+        var slideNavigationTransitionEffect = SlideNavigationTransitionEffect.FromBottom;
+
+        ContentFrame.Navigate(pageType, null, new SlideNavigationTransitionInfo() { Effect = slideNavigationTransitionEffect });
     }
 }
