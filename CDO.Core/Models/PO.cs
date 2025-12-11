8d469637eb346fcf923814fe1caac11fc5c63d69
Jon <me@jonalaiz.com>
2025-12-11 13:49:26 -0600

    Added Client property

--- CDO.Core/Models/PO.cs
+++ CDO.Core/Models/PO.cs
@@ -8,11 +8,11 @@ public record class PO(
     int? employerID,
     double? unitCost,
     string? unitOfMeasurement,
+    Client? client,
     DateTime startDate,
     DateTime endDate
     ) {
     public string? formattedStartDate => startDate.ToString(format: "MM/dd/yyyy");
     public string? formattedEndDate => endDate.ToString(format: "MM/dd/yyyy");
-
     public string? formattedCost => $"{unitCost:C2}";
 }
