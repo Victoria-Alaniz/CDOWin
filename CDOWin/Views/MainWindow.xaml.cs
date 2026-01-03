1a18cd70ebc504b28569266b61f1d142f05c9568
Jon <me@jonalaniz.com>
2026-01-03 04:01:09 -0600

    Cleaned up MainWindow

--- CDOWin/Views/MainWindow.xaml.cs
+++ CDOWin/Views/MainWindow.xaml.cs
@@ -4,71 +4,81 @@ using CDOWin.Views.Employers;
 using CDOWin.Views.Placements;
 using CDOWin.Views.Reminders;
 using CDOWin.Views.ServiceAuthorizations;
+using CommunityToolkit.WinUI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
 using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
 using WinUIEx;
 
 namespace CDOWin.Views;
 
 public sealed partial class MainWindow : Window {
-    int previousSelectedIndex = 0;
+    private int _previousSelectedIndex = 0;
 
+    // =========================
+    // Constructor
+    // =========================
     public MainWindow() {
         InitializeComponent();
         SetupWindow();
+        _ = SetupTitleBarAsync();
+
+        Activated += OnActivated;
     }
 
+    // =========================
+    // Window Setup
+    // =========================
+
     private void SetupWindow() {
-        // Setup our fulscreen window
         ExtendsContentIntoTitleBar = true;
 
-        // Setup Title bar
-        var uiSettings = new Windows.UI.ViewManagement.UISettings();
-        var accentColor = uiSettings.GetColorValue(Windows.UI.ViewManagement.UIColorType.Accent);
-        AppWindow.TitleBar.ButtonForegroundColor = accentColor;
-
-        // Set sizing and center the Window
         var manager = WindowManager.Get(this);
         manager.MinHeight = 800;
         manager.MinWidth = 1200;
-
-        SidebarFrame.Navigate(typeof(RemindersPage));
     }
 
-    private void NavigationLoaded(object sender, RoutedEventArgs e) {
-        NavigationBar.SelectedItem = NavigationBar.MenuItems[0];
+    private async Task SetupTitleBarAsync() {
+        await DispatcherQueue.EnqueueAsync(() => { });
+
+        var uiSettings = new UISettings();
+        var accentColor = uiSettings.GetColorValue(UIColorType.Accent);
+        AppWindow.TitleBar.ButtonForegroundColor = accentColor;
     }
+
+    private void OnActivated(object sender, WindowActivatedEventArgs args) {
+        Activated -= OnActivated;
+        NavigationBar.SelectedItem = NavigationBar.MenuItems.First();
+        SidebarFrame.Navigate(typeof(RemindersPage), null, new SlideNavigationTransitionInfo() { 
+            Effect = SlideNavigationTransitionEffect.FromBottom 
+        });
+    }
+
+    // =========================
+    // Navigation
+    // =========================
 
     private void NavigationSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args) {
         if (args.SelectedItem is NavigationViewItem selectedItem && selectedItem.Tag is CDOFrame frame) {
+            Type pageType = frame switch {
+                CDOFrame.Clients => typeof(ClientsPage),
+                CDOFrame.Counselors => typeof(CounselorsPage),
+                CDOFrame.Employers => typeof(EmployersPage),
+                CDOFrame.ServiceAuthorizations => typeof(ServiceAuthorizationsPage),
+                CDOFrame.Placements => typeof(PlacementsPage),
+                _ => throw new ArgumentOutOfRangeException(nameof(frame), frame, null),
+            };
+
             var currentSelectedIndex = sender.MenuItems.IndexOf(selectedItem);
-            System.Type pageType;
-
-            switch (frame) {
-                case CDOFrame.Clients:
-                    pageType = typeof(ClientsPage);
-                    break;
-                case CDOFrame.Counselors:
-                    pageType = typeof(CounselorsPage);
-                    break;
-                case CDOFrame.Employers:
-                    pageType = typeof(EmployersPage);
-                    break;
-                case CDOFrame.ServiceAuthorizations:
-                    pageType = typeof(ServiceAuthorizationsPage);
-                    break;
-                case CDOFrame.Placements:
-                    pageType = typeof(PlacementsPage);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(frame), frame, null);
-            }
-
-            var slideNavigationTransitionEffect = currentSelectedIndex - previousSelectedIndex > 0 ? SlideNavigationTransitionEffect.FromRight : SlideNavigationTransitionEffect.FromLeft;
-            ContentFrame.Navigate(pageType, null, new SlideNavigationTransitionInfo() { Effect = slideNavigationTransitionEffect });
-            previousSelectedIndex = currentSelectedIndex;
+            var effect = currentSelectedIndex - _previousSelectedIndex > 0
+                ? SlideNavigationTransitionEffect.FromRight
+                : SlideNavigationTransitionEffect.FromLeft;
+            ContentFrame.Navigate(pageType, null, new SlideNavigationTransitionInfo() { Effect = effect });
+            _previousSelectedIndex = currentSelectedIndex;
         }
     }
 }
